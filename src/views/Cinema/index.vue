f20a8b20a5744a4046bd5e60125f05d306a64b7e
ChillyBreeze1998 <1342327426@qq.com>
2019-06-26 23:51:54 +0800

    路由组件页面结构

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Header title="我的影院"/>
+    <div id="content">
+      <div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+    </div>
+    <Cinema-list></Cinema-list>
+    <TabBar/>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/Header";
+import TabBar from "../../components/TabBar";
+import CinemaList from '../../components/CinemaList'
+export default {
+  name: "Cinema",
+  components: {
+    Header,
+    TabBar,
+    CinemaList
+  }
+};
+</script>
+
+<style scope="this api replaced by slot-scope in 2.5.0+">
+#content {
+  margin-bottom: 0;
+}
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
