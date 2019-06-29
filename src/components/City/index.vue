373c46c55e0dd22c0a29a1932ec9d9a860fbc238
ChillyBreeze1998 <1342327426@qq.com>
2019-06-29 23:09:26 +0800

    真实数据渲染

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,107 +1,225 @@
 <template>
-			<div class="city_body">
-				<div class="city_list">
-					<div class="city_hot">
-						<h2>热门城市</h2>
-						<ul class="clearfix">
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-							<li>上海</li>
-							<li>北京</li>
-						</ul>
-					</div>
-					<div class="city_sort">
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>
-						<div>
-							<h2>A</h2>
-							<ul>
-								<li>阿拉善盟</li>
-								<li>鞍山</li>
-								<li>安庆</li>
-								<li>安阳</li>
-							</ul>
-						</div>
-						<div>
-							<h2>B</h2>
-							<ul>
-								<li>北京</li>
-								<li>保定</li>
-								<li>蚌埠</li>
-								<li>包头</li>
-							</ul>
-						</div>	
-					</div>
-				</div>
-				<div class="city_index">
-					<ul>
-						<li>A</li>
-						<li>B</li>
-						<li>C</li>
-						<li>D</li>
-						<li>E</li>
-					</ul>
-				</div>
-			</div>
+  <div class="city_body">
+    <!-- <div class="city_list">
+      <div class="city_hot">
+        <h2>热门城市</h2>
+        <ul class="clearfix">
+          <li>上海</li>
+        </ul>
+      </div>
+      <div class="city_sort">
+        <div>
+          <h2>A</h2>
+          <ul>
+            <li>阿拉善盟</li>
+            <li>鞍山</li>
+            <li>安庆</li>
+            <li>安阳</li>
+          </ul>
+      
+        </div>
+      </div>
+    </div>
+    <div class="city_index">
+      <ul>
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+        <li>E</li>
+      </ul>
+    </div>
+    </div>-->
+    <div class="city_list">
+      <div class="city_hot">
+        <h2>热门城市</h2>
+        <ul class="clearfix">
+          <li v-for="item in hotList" :key="item">{{item.nm}}</li>
+        </ul>
+      </div>
+      <div class="city_sort" ref="city_sort">
+        <div v-for="item in cityList" :key="item.index">
+          <h2>{{ item.index }}</h2>
+          <ul>
+            <li
+              v-for="itemList in item.list"
+              :key="itemList.id"
+              @tap="handleToCity(itemList.nm , itemList.id)"
+            >{{ itemList.nm }}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+       <div class="city_index">
+            <ul>
+                <li v-for="(item,index) in cityList" :key="item.index" @touchstart="handleToIndex(index)">{{ item.index }}</li>
+            </ul>
+        </div>
+  </div>
 </template>
 
 <script>
+import { log } from "util";
 export default {
-  name: 'City'
+  name: "City",
+  data() {
+    return {
+      cityList: [],
+      hotList: [],
+      isLoading: true
+    };
+  },
+  mounted() {
+    this.axios.get("/api/cityList").then(res => {
+      console.log(res);
+
+      let msg = res.data.msg;
+      if (msg === "ok") {
+        var cities = res.data.data.cities;
+
+        var { cityList, hotList } = this.formatCityList(cities);
+        this.cityList = cityList;
+        this.hotList = hotList;
+      }
+    });
+  },
+  methods: {
+    // 格式化城市列表的方法
+    formatCityList(cities) {
+      var cityList = [];
+      var hotList = [];
+
+      for (let index = 0; index < cities.length; index++) {
+        if (cities[index].isHot === 1) {
+          hotList.push(cities[index]);
+        }
+      }
+
+      for (let index = 0; index < cities.length; index++) {
+        // 把每个列表的第一个拼音的字母保存到这个变量里 转换为大写
+        var firstLetter = cities[index].py.substring(0, 1).toUpperCase();
+        // 如果有tocom这个函数的值则新添加
+        if (toCom(firstLetter)) {
+          cityList.push({
+            index: firstLetter,
+            list: [{ nm: cities[index].nm, id: cities[index].id }]
+          });
+          //  console.log('cityList',cityList);
+        } else {
+          //累加到已有index中
+          for (var j = 0; j < cityList.length; j++) {
+            if (cityList[j].index === firstLetter) {
+              cityList[j].list.push({
+                nm: cities[index].nm,
+                id: cities[index].id
+              });
+            }
+          }
+        }
+      }
+      cityList.sort((n1, n2) => {
+        if (n1.index > n2.index) {
+          return 1;
+        } else if (n1.index < n2.index) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+      console.log(cityList);
+
+      // 比较index和firstLetter
+      function toCom(firstLetter) {
+        // 遍历cityList
+        for (let index = 0; index < cityList.length; index++) {
+          // 如果比对成功的话就停止继续比对 也就是return false
+          if (cityList[index].index === firstLetter) {
+            // console.log(cityList[index].index, firstLetter);
+            return false;
+          }
+        }
+        return true;
+      }
+      return {
+        cityList,
+        hotList
+      };
+    },
+    handleToIndex(index) {
+      var h2 = this.$refs.city_sort.getElementsByTagName('h2');
+       this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop-50;
+    }
+  }
 };
 </script>
+  
 
 <style scope>
-.city_body{ margin-top: 45px; display: flex; width:100%; position: absolute; top: 0; bottom: 0;}
-.city_body .city_list{ flex:1; overflow: auto; background: #FFF5F0;}
-.city_body .city_list::-webkit-scrollbar{
-    background-color:transparent;
-    width:0;
+.city_body {
+  margin-top: 45px;
+  display: flex;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
 }
-.city_body .city_hot{ margin-top: 20px;}
-.city_body .city_hot h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
-.city_body .city_hot ul li{ float: left; background: #fff; width: 29%; height: 33px; margin-top: 15px; margin-left: 3%; padding: 0 4px; border: 1px solid #e6e6e6; border-radius: 3px; line-height: 33px; text-align: center; box-sizing: border-box;}
-.city_body .city_sort div{ margin-top: 20px;}
-.city_body .city_sort h2{ padding-left: 15px; line-height: 30px; font-size: 14px; background:#F0F0F0; font-weight: normal;}
-.city_body .city_sort ul{ padding-left: 10px; margin-top: 10px;}
-.city_body .city_sort ul li{ line-height: 30px; line-height: 30px;}
-.city_body .city_index{ width:20px; display: flex; flex-direction:column; justify-content:center; text-align: center; border-left:1px #e6e6e6 solid;}
+.city_body .city_list {
+  flex: 1;
+  overflow: auto;
+  background: #fff5f0;
+}
+.city_body .city_list::-webkit-scrollbar {
+  background-color: transparent;
+  width: 0;
+}
+.city_body .city_hot {
+  margin-top: 20px;
+}
+.city_body .city_hot h2 {
+  padding-left: 15px;
+  line-height: 30px;
+  font-size: 14px;
+  background: #f0f0f0;
+  font-weight: normal;
+}
+.city_body .city_hot ul li {
+  float: left;
+  background: #fff;
+  width: 29%;
+  height: 33px;
+  margin-top: 15px;
+  margin-left: 3%;
+  padding: 0 4px;
+  border: 1px solid #e6e6e6;
+  border-radius: 3px;
+  line-height: 33px;
+  text-align: center;
+  box-sizing: border-box;
+}
+.city_body .city_sort div {
+  margin-top: 20px;
+}
+.city_body .city_sort h2 {
+  padding-left: 15px;
+  line-height: 30px;
+  font-size: 14px;
+  background: #f0f0f0;
+  font-weight: normal;
+}
+.city_body .city_sort ul {
+  padding-left: 10px;
+  margin-top: 10px;
+}
+.city_body .city_sort ul li {
+  line-height: 30px;
+  line-height: 30px;
+}
+.city_body .city_index {
+  width: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  border-left: 1px #e6e6e6 solid;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routers'
 import store from './stores'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.filter('setWH',(url , arg)=>{
+  return url.replace(/w\.h/,arg);
+});
 
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
